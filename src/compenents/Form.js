71405b99ccddd59fd30987f610bb3f8ce0d0ee51
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-06 15:59:14 +0530

    added new states

--- src/compenents/Form.js
+++ src/compenents/Form.js
@@ -5,15 +5,31 @@ import '../index.css';
 import { useState } from "react";
 
 
+const initialUserInput = {
+    "current-savings": 50000,
+    "yearly-contribution": 5000,
+    "expected-return": 7,
+    "duration": 5,
+}
+
 function Form(props) {
+    const [userInput, setUserInput] = useState(initialUserInput);
     const submitHandler = (e) => {
         e.preventDefault();
         console.log("submitted");
     }
     const resetHandler = (e) => {
-        e.preventDefault();
-        console.log("reset");
+        setUserInput(initialUserInput);
     }
+    const inputSubmitHandler = (input,value) => {
+        setUserInput((prevInput) =>{
+            return{
+                ...prevInput,
+                [input]:value,
+            }}
+        )
+    }
+
     const {
         currentSaving,
         setCurrentSaving,
@@ -28,23 +44,60 @@ function Form(props) {
         calculateHandler,
     } = props;
     return (
-        <form onSubmit= {submitHandler} className="form" >
-            <InputGroup
-                currentSaving={currentSaving}
-                yearlyContribution={yearlyContribution}
-                setCurrentSaving={setCurrentSaving}
-                setYearlyContribution={setYearlyContribution}
-            />
-            <InterestInputGroup
-                expectedInterest={expectedInterest}
-                investmentDuration={investmentDuration}
-                setExpectedInterest={setExpectedInterest}
-                setInvestmentDuration={setInvestmentDuration}
-            />
-            <Action
-                setTakeAction={setTakeAction}
-                onClick={calculateHandler} />
-        </form>
+        <form onSubmit={submitHandler} className="form">
+            <div className="input-group">
+                <p>
+                <label htmlFor="current-savings">Current Savings ($)</label>
+                <input
+                    type="number"
+                    id="current-savings"
+                    onChange={(e) => inputSubmitHandler("current-savings",e.target.value)}
+                    value={userInput["current-savings"]}
+                />
+                </p>
+                <p>
+                <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+                <input
+                    type="number"
+                    id="yearly-contribution"
+                    value={userInput["yearly-contribution"]}
+                    onChange={(e) => inputSubmitHandler("yearly-contribution",e.target.value)}
+                />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                <label htmlFor="expected-return">Expected Interest (% per year)</label>
+                <input
+                    type="number"
+                    id="expected-return"
+                    value={userInput["expected-return"]}
+                    onChange={(e) => inputSubmitHandler("expected-return",e.target.value)}
+                />
+                </p>
+                <p>
+                <label htmlFor="duration">Investment Duration (years)</label>
+                <input
+                    type="number"
+                    id="duration"
+                    value={userInput["duration"]}
+                    onChange={(e) => inputSubmitHandler("duration",e.target.value)}
+                />
+                </p>
+            </div>
+            <p className="actions">
+                <button
+                    type="reset"
+                    className="buttonAlt"
+                    onClick={resetHandler}
+                >
+                Reset
+                </button>
+                <button type="submit" className="button">
+                Calculate
+                </button>
+            </p>
+</form>
     )
 }
 export default Form;
